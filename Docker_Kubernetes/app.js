ba119ade5936b40dddcd39f3a9ba76d70f174bbb
snadi043 <snadi043@uottawa.ca>
2025-07-23 10:09:34 -0400

    Re-initiated the Kuberenetes Project with the new name "Docker-Kuberenetes" under the "Docker360" repository.

---
+++ Docker_Kubernetes/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`
+        <h1>Hello from this NODEJS app!</h1>
+        <p>Adding a new line to restart the pods in kubernetes application...</p>
+        <p>Try sending a request to /error and see what happens next.</p>
+    `);
+});
+
+app.get('/error', (req, res) => {
+    process.exit(1);
+});
+
+app.listen(8080);
